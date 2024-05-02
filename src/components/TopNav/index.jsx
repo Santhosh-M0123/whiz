accf21756fee9863b87d1d2dd08822a1927fee8b
WHIZLABS <whizlabs@WHIZLABSs-Laptop.local>
2024-05-02 15:21:11 +0530

    logo navigation checked

--- src/components/TopNav/index.jsx
+++ src/components/TopNav/index.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import weatherLogo from "../../assests/images/weather_logo.webp";
 import "./index.css";
+import { Link } from "react-router-dom";
 
 const TopNav = () => {
   return (
     <div className="top-nav-container">
       <div className="top-nav-left">
-        {/* <div className="logo-container">
-                <img src={weatherLogo} alt='weather_logo'/>
-            </div> */}
         <div className="logo">
-          <h2>
-            <span className="logo_char">W</span>hiz
-          </h2>
+          <Link to={"/"} className="link" style={{ color: "black" }}>
+            <h2>
+              <span className="logo_char">W</span>hiz
+            </h2>
+          </Link>
         </div>
       </div>
       {/* <div className="top-nav-right"></div> */}
